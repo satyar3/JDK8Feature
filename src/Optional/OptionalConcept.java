d04488830083cfdf3b6d13c35dbbb9a0691453af
Satyaranjan Muduli <satyaranjan.muduli@amadeus.com>
2020-06-04 19:57:21 +0530

    Added optional concept

---
+++ src/Optional/OptionalConcept.java
@@ -0,0 +1,40 @@
+package Optional;
+
+import java.util.Optional;
+
+public class OptionalConcept
+{
+	public static void main(String[] args)
+	{
+		//Create optional ref
+		Optional<String> optionRef = Optional.empty();		
+		
+		//Create ref for which you want to check optional
+		String book = null;
+		
+		//OfNullable Returns an Optional describing the specified value, if non-null,otherwise returns an empty Optional.
+		optionRef = Optional.ofNullable(book);	
+		
+		//isPresent() returns true if value is not null
+		if(optionRef.isPresent())
+			System.out.println(optionRef.get());
+		else
+			System.out.println(optionRef);
+		
+		
+		/*
+		
+		//Of() Returns an Optional with the specified present non-null value and if it's null it'll throw nullpointer exception
+		optionRef = Optional.of(book);
+		if(optionRef.isPresent())
+			System.out.println(optionRef.get());
+		
+		*/
+		
+		//Set value using orElse() --> If the ref is null then set he value	and assign it to the ref
+		String language = null;
+		language = optionRef.ofNullable(language).orElse("String has been assigned");
+		System.out.println(language);
+		
+	}
+}
