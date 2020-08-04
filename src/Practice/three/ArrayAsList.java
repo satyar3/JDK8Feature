6368832a45ce5db22f41dd4cbabc21742fd27a85
Satyaranjan Muduli <satyaranjan.muduli@amadeus.com>
2020-08-04 21:23:44 +0530

    Practice added

---
+++ src/Practice/three/ArrayAsList.java
@@ -0,0 +1,35 @@
+package Practice.three;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class ArrayAsList
+{
+	public static void main(String[] args)
+	{
+		String colType = "Load";		
+		String colName = "F Accepted Cargo count";
+		
+		
+		
+		String[] colTypeArr = colType.split(" ");
+		colTypeArr[0] = colTypeArr[0].toLowerCase();
+		String updatedColType = Arrays.stream(colTypeArr).reduce((x,y) -> (x+y)).orElse("");
+
+
+		String[] colNameArr = colName.split(" ");
+		IntStream.range(0, colTypeArr.length).forEach(i -> colNameArr[i] = StringUtils.capitalize(colNameArr[i].toLowerCase()));
+		String updatedColName = Arrays.asList(colNameArr).stream().reduce((x,y) -> (x+y)).orElse("");
+
+
+		String colPath = "[row-id ='"+updatedColType+""+updatedColName+"'] .form-check >input";
+		System.out.println("Checkbox path"+ colPath);
+		
+		List<String> nonPrimaryList = Arrays.asList("FRT", "CAC", "BAG", "BUS", "FUE", "BUS", "TOW", "ICE", "CAT", "LCT", "FUE", "PRI", "CLN", "FUE", "PUB").stream().filter(x -> !x.equals("PRI")).collect(Collectors.toList());
+		IntStream.range(0,nonPrimaryList.size()).forEach(i -> System.out.println(nonPrimaryList.get(i)));
+	}
+}
